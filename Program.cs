bcc1a11845a0c0c71ae86a7a57d80cdc0ace046c
deepakporwal <er.deepakporwal@gmail.com>
2021-06-16 20:48:19 +0530

    Update Program.cs
    sum function added

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace SampleConsole
                 Console.WriteLine(i);
             }
             Console.WriteLine("New comment added by deepak");
+            
+            static int Sum(int x,int y)
+            {
+                return x+ y;
+            }
         }
     }
 }
